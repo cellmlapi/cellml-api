6cea59440781d4fbac68a1a00d08a45c046d99d7
amil082 <none@none>
2006-03-02 00:20:37 +0000

    Wrap all the threading code in ENABLE_THREADSAFETY so as to not burden users who don't want it(which is probably most people, since it isn't finished yet).
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%4049

--- sources/ThreadWrapper.hxx
+++ sources/ThreadWrapper.hxx
@@ -4,27 +4,37 @@ class CDAMutex
 public:
   CDAMutex()
   {
+#ifdef ENABLE_THREADSAFETY
     if (!g_thread_supported())
       g_thread_init(NULL);
     gmutex = g_mutex_new();
+#endif
   }
 
   ~CDAMutex()
   {
+#ifdef ENABLE_THREADSAFETY
     g_mutex_free(gmutex);
+#endif
   }
 
   void Lock()
   {
+#ifdef ENABLE_THREADSAFETY
     g_mutex_lock(gmutex);
+#endif
   }
 
   void Unlock()
   {
+#ifdef ENABLE_THREADSAFETY
     g_mutex_unlock(gmutex);
+#endif
   }
 private:
+#ifdef ENABLE_THREADSAFETY
   GMutex* gmutex;
+#endif
 };
 
 // A class to provide a scoped lock...
