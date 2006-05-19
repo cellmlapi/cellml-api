f2082c5fe9e9cf9f037b8ce8a0a61afc8a07223a
amil082 <none@none>
2006-05-19 00:00:22 +0000

    Make building of most of the code optional.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40280

--- sources/cellml_corba_server/build.mk
+++ sources/cellml_corba_server/build.mk
@@ -1,6 +1,6 @@
 # CellML CORBA Server...
 bin_PROGRAMS := cellml_corba_server
-cellml_corba_server_SOURCES := sources/cellml_corba_server/Main.cpp
+cellml_corba_server_SOURCES := $(top_srcdir)/sources/cellml_corba_server/Main.cpp
 cellml_corba_server_CXXFLAGS := \
   -I$(top_srcdir) -I$(top_srcdir)/sources -I$(top_srcdir)/sources/cellml \
   -I $(top_srcdir)/sources/dom -I $(top_srcdir)/sources/dom_direct \
