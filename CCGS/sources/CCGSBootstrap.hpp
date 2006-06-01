c487e312fde92f05a5945f74dad318333ec43a6a
amil082 <none@none>
2006-06-01 02:52:26 +0000

    Add the CCGSBootstrap, allowing easy programmatic access from C++.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40298

---
+++ CCGS/sources/CCGSBootstrap.hpp
@@ -0,0 +1,6 @@
+/**
+ * Creates a new C generator.
+ * This code is the external C++ interface which allows you to fetch the
+ * implementation.
+ */
+iface::cellml_services::CGenerator* CreateCGenerator(void);
