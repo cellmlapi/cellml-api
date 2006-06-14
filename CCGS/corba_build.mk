b88bee96a11f6f0489bcd5667a6c78e8520e6911
amil082 <none@none>
2006-06-14 03:41:23 +0000

    Update sources needed to support CORBA.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40321

--- CCGS/corba_build.mk
+++ CCGS/corba_build.mk
@@ -1,5 +1,10 @@
 bin_PROGRAMS += CCGSService
-CCGSService_SOURCES := $(top_srcdir)/CCGS/sources/CCGSServiceRegistration.cpp
+CCGSService_SOURCES := \
+  $(top_srcdir)/CCGS/sources/CCGSServiceRegistration.cpp \
+  $(top_builddir)/interfaces/CCICCGS.cxx \
+  $(top_builddir)/interfaces/SCICCGS.cxx \
+  $(top_builddir)/interfaces/CCGSSK.cc
+
 CCGSService_LDADD := \
   $(top_builddir)/libccgs.la \
   $(top_builddir)/libcellml_corba_client.la \
@@ -13,4 +18,10 @@ CCGSService_CXXFLAGS := \
 	-I$(top_builddir)/interfaces \
 	-I$(top_srcdir) \
 	-I$(top_srcdir)/sources/cellml_corba_server \
-        -I$(top_srcdir)/sources/
+        -I$(top_srcdir)/sources/ \
+	-I$(top_srcdir)/simple_interface_generators/glue
+
+BUILT_SOURCES += \
+  $(top_builddir)/interfaces/CCICCGS.cxx \
+  $(top_builddir)/interfaces/SCICCGS.cxx \
+  $(top_builddir)/interfaces/CCGSSK.cc
