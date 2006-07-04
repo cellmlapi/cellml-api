d3b50c8fadbd6db29750ac7a41c3acbf696b9d85
amil082 <none@none>
2006-07-04 00:53:04 +0000

    Tidy up whitespace.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40366

--- tests/build.mk
+++ tests/build.mk
@@ -1,10 +1,12 @@
 # Tests...
 check_PROGRAMS=tests/RunTestBin
 TESTS=tests/RunTestBin
-tests_RunTestBin_SOURCES=tests/TestOutput.cpp \
-			 tests/DOMTest.cpp \
-                         tests/MathMLTest.cpp \
-                         tests/CellMLTest.cpp \
-                         tests/CellMLEventsTest.cpp
+tests_RunTestBin_SOURCES=\
+	tests/TestOutput.cpp \
+	tests/MathMLTest.cpp \
+	tests/DOMTest.cpp \
+	tests/CellMLTest.cpp \
+	tests/CellMLEventsTest.cpp
+
 tests_RunTestBin_LDADD=-L$(top_builddir)/sources -lcellml -lcppunit -lxml2 -ldl
 tests_RunTestBin_CXXFLAGS=-I$(top_srcdir)/sources -I$(top_srcdir)/sources/dom/ -I$(top_srcdir)/sources/mathml/ -I$(top_srcdir)/sources/cellml -I$(top_builddir)/interfaces -I$(top_srcdir)/tests
