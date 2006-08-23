9371d66ced3474a9a55b6564cabcb997544ec9d5
amil082 <none@none>
2006-08-23 03:29:08 +0000

    Need a -no-undefined to make this work on Win32.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40527

--- interfaces/corba_build.mk
+++ interfaces/corba_build.mk
@@ -28,7 +28,7 @@ libcellml_corba_bridge_la_CXXFLAGS := \
   -I$(top_builddir)/interfaces -I$(top_srcdir) -I$(top_srcdir)/sources -I$(top_srcdir)/simple_interface_generators/glue $(AM_CXXFLAGS)
 
 libcellml_corba_stubs_la_CXXFLAGS := \
-  -I$(top_builddir)/interfaces -I$(top_srcdir)/simple_interface_generators/glue
+  -I$(top_builddir)/interfaces -I$(top_srcdir)/simple_interface_generators/glue -no-undefined
 
 libcellml_corba_bridge_la_LIBADD := libcellml_corba_stubs.la $(STLLINK)
 
