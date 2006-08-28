676c17e7e9f50b8f8f84996df15ef344a738af35
amil082 <none@none>
2006-08-28 20:05:48 +0000

    MingW32 doesn't like / on the end of include paths.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40538

--- sources/cellml_context/build.mk
+++ sources/cellml_context/build.mk
@@ -8,7 +8,7 @@ lib_LTLIBRARIES += libcellml_context.la
 libcellml_context_la_SOURCES := \
   $(top_srcdir)/sources/cellml_context/CellMLContextImplementation.cpp
 
-libcellml_context_la_CXXFLAGS = -I$(top_builddir)/interfaces/ -I$(top_srcdir)/sources -I$(top_srcdir)/include/CellMLBootstrap.hxx -I$(top_srcdir)/sources/cellml $(AM_CXXFLAGS)
+libcellml_context_la_CXXFLAGS = -I$(top_builddir)/interfaces -I$(top_srcdir)/sources -I$(top_srcdir)/sources/cellml $(AM_CXXFLAGS)
 
 libcellml_context_la_LIBADD := \
   libcellml.la \
