99b7fa786040e688f4c4be0cc1b2e4cd6ee6651f
amil082 <none@none>
2006-08-28 20:44:53 +0000

    Add missing -lgsl.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40540

--- CIS/build.mk
+++ CIS/build.mk
@@ -4,7 +4,7 @@ libcis_la_SOURCES := \
   $(top_srcdir)/CIS/sources/CISSolve.cxx
 
 libcis_la_LIBADD := \
-  $(top_builddir)/libccgs.la $(STLLINK)
+  $(top_builddir)/libccgs.la -lgsl $(STLLINK)
 
 libcis_la_CXXFLAGS := \
   -Wall -ggdb -I$(top_srcdir)/sources -I$(top_builddir)/interfaces $(AM_CXXFLAGS)
