da83e1347a6c74fddf4588f2b33dee5e6faf2a29
amil082 <none@none>
2006-08-29 21:52:47 +0000

    Remove trailing / from -I path.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40562

--- tests/build.mk
+++ tests/build.mk
@@ -9,4 +9,4 @@ tests_RunTestBin_SOURCES=\
 	tests/CellMLEventsTest.cpp
 
 tests_RunTestBin_LDADD=-L$(top_builddir) -lcellml -lcppunit -lxml2
-tests_RunTestBin_CXXFLAGS=-I$(top_srcdir)/sources -I$(top_srcdir)/sources/dom/ -I$(top_srcdir)/sources/mathml/ -I$(top_srcdir)/sources/cellml -I$(top_builddir)/interfaces -I$(top_srcdir)/tests
+tests_RunTestBin_CXXFLAGS=-I$(top_srcdir)/sources -I$(top_srcdir)/sources/dom -I$(top_srcdir)/sources/mathml -I$(top_srcdir)/sources/cellml -I$(top_builddir)/interfaces -I$(top_srcdir)/tests
