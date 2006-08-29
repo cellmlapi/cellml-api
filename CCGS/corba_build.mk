e23525358eacb13d1787c4fd4699c35b30b6b16c
amil082 <none@none>
2006-08-29 01:14:37 +0000

    Another split into stubs and bridge for Win32.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40551

--- CCGS/corba_build.mk
+++ CCGS/corba_build.mk
@@ -1,11 +1,10 @@
-lib_LTLIBRARIES += libccgs_corba.la libCCGSService.la
-noinst_LTLIBRARIES += libccgs_corba_sk.la
+lib_LTLIBRARIES += libccgs_corba_bridge.la libccgs_corba_stubs.la libCCGSService.la
 
-libccgs_corba_la_SOURCES := \
+libccgs_corba_bridge_la_SOURCES := \
   $(top_builddir)/interfaces/CCICCGS.cxx \
   $(top_builddir)/interfaces/SCICCGS.cxx
 
-libccgs_corba_sk_la_SOURCES := \
+libccgs_corba_stubs_la_SOURCES := \
   $(top_builddir)/interfaces/CCGSSK.cc
 
 libCCGSService_la_SOURCES := \
@@ -13,7 +12,9 @@ libCCGSService_la_SOURCES := \
 
 libCCGSService_la_LIBADD := \
   $(top_builddir)/libccgs.la \
-  $(top_builddir)/libccgs_corba.la
+  $(top_builddir)/libccgs_corba_bridge.la \
+  $(top_builddir)/libccgs_corba_stubs.la
+
 libCCGSService_la_LDFLAGS := -no-undefined -shared -module $(OMNILINK)
 
 libCCGSService_la_CXXFLAGS := \
@@ -23,21 +24,28 @@ libCCGSService_la_CXXFLAGS := \
         -I$(top_srcdir)/sources \
 	-I$(top_srcdir)/simple_interface_generators/glue $(AM_CXXFLAGS)
 
-libccgs_corba_la_CXXFLAGS := \
+libccgs_corba_bridge_la_CXXFLAGS := \
 	-I$(top_builddir)/interfaces \
 	-I$(top_srcdir) \
 	-I$(top_srcdir)/sources/cellml_corba_server \
         -I$(top_srcdir)/sources \
 	-I$(top_srcdir)/simple_interface_generators/glue $(AM_CXXFLAGS)
 
-libccgs_corba_sk_la_CXXFLAGS := \
+libccgs_corba_stubs_la_CXXFLAGS := \
 	-I$(top_builddir)/interfaces \
 	-I$(top_srcdir)/simple_interface_generators/glue
 
-libccgs_corba_la_LIBADD := \
-  $(top_builddir)/libccgs_corba_sk.la
+libccgs_corba_bridge_la_LIBADD := \
+  $(top_builddir)/libccgs_corba_stubs.la \
+  $(top_builddir)/libCORBASupport.la
 
-libccgs_corba_la_LDFLAGS := \
+libccgs_corba_stubs_la_LIBADD := \
+  $(OMNILINK) $(STLLINK) $(top_builddir)/libcellml_corba_stubs.la
+
+libccgs_corba_bridge_la_LDFLAGS := \
+  -no-undefined
+
+libccgs_corba_stubs_la_LDFLAGS := \
   -no-undefined
 
 BUILT_SOURCES += \
