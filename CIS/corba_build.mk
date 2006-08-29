7dbe459f71ebacd3f2f12a01b0453dcdcbbc5af4
amil082 <none@none>
2006-08-29 03:23:49 +0000

    Fix the CIS for Win32.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40553

--- CIS/corba_build.mk
+++ CIS/corba_build.mk
@@ -1,36 +1,28 @@
-lib_LTLIBRARIES += libcis_corba.la libCISService.la
+lib_LTLIBRARIES += \
+  libcis_corba_bridge.la libcis_corba_stubs.la libCISService.la
 
-libcis_corba_la_SOURCES := \
+libcis_corba_bridge_la_SOURCES := \
   $(top_builddir)/interfaces/CCICIS.cxx \
-  $(top_builddir)/interfaces/SCICIS.cxx \
-  $(top_builddir)/interfaces/CISSK.cc
+  $(top_builddir)/interfaces/SCICIS.cxx
 
-libcis_corba_la_LIDADD := \
-  $(top_builddir)/libcellml.la \
-  $(top_builddir)/libcellml_corba_bridge.la \
-  $(top_builddir)/libcis.la \
-  $(top_builddir)/libccgs.la \
-  $(top_builddir)/libccgs_corba.la
+libcis_corba_stubs_la_SOURCES := \
+  $(top_builddir)/interfaces/CISSK.cc
 
 libCISService_la_SOURCES := \
   $(top_srcdir)/CIS/sources/CISServiceRegistration.cpp
 
+libCISService_la_LDFLAGS := -shared -module -no-undefined $(OMNILINK) -lgsl -lgslcblas
+
 libCISService_la_LIBADD := \
   $(top_builddir)/libcellml.la \
   $(top_builddir)/libcellml_corba_bridge.la \
+  $(top_builddir)/libcellml_corba_stubs.la \
   $(top_builddir)/libcis.la \
-  $(top_builddir)/libcis_corba.la \
+  $(top_builddir)/libcis_corba_bridge.la \
+  $(top_builddir)/libcis_corba_stubs.la \
   $(top_builddir)/libccgs.la \
-  $(top_builddir)/libccgs_corba.la
-
-libCISService_la_LDFLAGS := -shared -module $(OMNILINK) -lgsl -lgslcblas
-
-libcis_corba_la_CXXFLAGS := \
-	-I$(top_builddir)/interfaces \
-	-I$(top_srcdir) \
-	-I$(top_srcdir)/sources/cellml_corba_server \
-        -I$(top_srcdir)/sources \
-	-I$(top_srcdir)/simple_interface_generators/glue $(AM_CXXFLAGS)
+  $(top_builddir)/libccgs_corba_bridge.la \
+  $(top_builddir)/libccgs_corba_stubs.la
 
 libCISService_la_CXXFLAGS := \
 	-I$(top_builddir)/interfaces \
@@ -38,6 +30,29 @@ libCISService_la_CXXFLAGS := \
 	-I$(top_srcdir)/sources/cellml_corba_server \
         -I$(top_srcdir)/sources \
 	-I$(top_srcdir)/simple_interface_generators/glue $(AM_CXXFLAGS)
+
+libcis_corba_bridge_la_CXXFLAGS := \
+	-I$(top_builddir)/interfaces \
+	-I$(top_srcdir) \
+	-I$(top_srcdir)/sources/cellml_corba_server \
+        -I$(top_srcdir)/sources \
+	-I$(top_srcdir)/simple_interface_generators/glue $(AM_CXXFLAGS)
+
+libcis_corba_stubs_la_CXXFLAGS := \
+	-I$(top_builddir)/interfaces \
+	-I$(top_srcdir)/simple_interface_generators/glue
+
+libcis_corba_bridge_la_LIBADD := \
+  $(top_builddir)/libcis_corba_stubs.la \
+  $(top_builddir)/libccgs_corba_bridge.la \
+  $(top_builddir)/libcellml_corba_bridge.la \
+  $(top_builddir)/libCORBASupport.la
+
+libcis_corba_stubs_la_LIBADD := \
+  $(OMNILINK) \
+  $(STLLINK) \
+  $(top_builddir)/libcellml_corba_stubs.la \
+  $(top_builddir)/libccgs_corba_stubs.la
 
 BUILT_SOURCES += \
   $(top_builddir)/interfaces/CCICIS.cxx \
