d9c2466fb977de64c45964e32c5807501a77d0b2
amil082 <none@none>
2006-09-18 21:03:56 +0000

    Utilise a gcc attribute so we can detect (at compile time) leaks caused by functions like appendChild (which return a value, but also have side effects) being used without releasing the return value.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%40668

--- sources/cda_compiler_support.h
+++ sources/cda_compiler_support.h
@@ -4,13 +4,16 @@
 #include "cda_config.h"
 #ifdef HAVE_VISIBILITY_ATTRIBUTE
 #define CDA_EXPORT __attribute__((visibility("default")))
+#define WARN_IF_RETURN_UNUSED __attribute__((warn_unused_result))
 #define CDA_IMPORT __attribute__((visibility("default")))
 #elif defined(WIN32)
 #define CDA_EXPORT __declspec(dllexport)
 #define CDA_IMPORT __declspec(dllimport)
+#define WARN_IF_RETURN_UNUSED
 #else
 #define CDA_EXPORT
 #define CDA_IMPORT
+#define WARN_IF_RETURN_UNUSED
 #endif
 
 #endif // compiler_support_h
