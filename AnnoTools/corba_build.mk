fe72c25f7f80320fce19369b46afae78ae2321c9
amil082 <none@none>
2007-08-01 03:27:36 +0000

    We don't have an AnnoToolService registration yet so don't try to build the service.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%401430

--- AnnoTools/corba_build.mk
+++ AnnoTools/corba_build.mk
@@ -1,5 +1,5 @@
 lib_LTLIBRARIES += \
-  libannotools_corba_bridge.la libannotools_corba_stubs.la libAnnoToolsService.la
+  libannotools_corba_bridge.la libannotools_corba_stubs.la
 
 libannotools_corba_bridge_la_SOURCES := \
   $(top_builddir)/interfaces/CCIAnnoTools.cxx \
@@ -7,29 +7,6 @@ libannotools_corba_bridge_la_SOURCES := \
 
 libannotools_corba_stubs_la_SOURCES := \
   $(top_builddir)/interfaces/AnnoToolsSK.cc
-
-libAnnoToolsService_la_SOURCES := \
-  $(top_srcdir)/AnnoTools/sources/AnnoToolsServiceRegistration.cpp
-
-libAnnoToolsService_la_LDFLAGS := -shared -module -no-undefined $(OMNILINK) -lgsl -lgslcblas
-
-libAnnoToolsService_la_LIBADD := \
-  $(top_builddir)/libcellml.la \
-  $(top_builddir)/libcellml_corba_bridge.la \
-  $(top_builddir)/libcellml_corba_stubs.la \
-  $(top_builddir)/libannotools.la \
-  $(top_builddir)/libannotools_corba_bridge.la \
-  $(top_builddir)/libannotools_corba_stubs.la \
-  $(top_builddir)/libccgs.la \
-  $(top_builddir)/libccgs_corba_bridge.la \
-  $(top_builddir)/libccgs_corba_stubs.la
-
-libAnnoToolsService_la_CXXFLAGS := \
-	-I$(top_builddir)/interfaces \
-	-I$(top_srcdir) \
-	-I$(top_srcdir)/sources/cellml_corba_server \
-        -I$(top_srcdir)/sources \
-	-I$(top_srcdir)/simple_interface_generators/glue $(AM_CXXFLAGS)
 
 libannotools_corba_bridge_la_CXXFLAGS := \
 	-I$(top_builddir)/interfaces \
