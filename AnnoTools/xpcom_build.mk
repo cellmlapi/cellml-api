d6edd76db3352df9c6a1f5a73425d11c0ef74829
amil082 <none@none>
2007-08-01 21:37:24 +0000

    Remove extraneous libraries (which we get through dependencies anyway) because they break the OSX build.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%401434

--- AnnoTools/xpcom_build.mk
+++ AnnoTools/xpcom_build.mk
@@ -9,10 +9,8 @@ libannotools_xpcom_bridge_la_SOURCES = \
 
 libannotools_xpcom_bridge_la_LIBADD = \
   $(STLLINK) \
-  $(top_builddir)/libcellml.la \
   $(top_builddir)/libcellml_xpcom_bridge.la \
-  $(top_builddir)/libannotools.la \
-  $(top_builddir)/libXPCOMSupport.la
+  $(top_builddir)/libannotools.la
 
 libannotools_xpcom_bridge_la_LDFLAGS = \
   -no-undefined
