66a07d9327bf81d3b7f156cf81ef942fe023e493
amil082 <none@none>
2007-09-21 02:24:51 +0000

    Added an extra space to the default CCGS MAL specification (tracker item 172) and added a testcase that checks we do the right thing when we have binary and unary minuses combined with negative constants.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%401773

---
+++ tests/test_expected/number-minus.c
@@ -0,0 +1,30 @@
+/* Model is correctly constrained.
+ * No equations needed Newton-Raphson evaluation.
+ * The rate and state arrays need 1 entries.
+ * The algebraic variables array needs 0 entries.
+ * The constant array needs 0 entries.
+ * Variable storage is as follows:
+ * * Target d^1/dt^1 distance in component main
+ * * * Variable type: algebraic variable
+ * * * Variable index: 0
+ * * * Variable storage: RATES[0]
+ * * Target distance in component main
+ * * * Variable type: state variable
+ * * * Variable index: 0
+ * * * Variable storage: STATES[0]
+ * * Target time in component main
+ * * * Variable type: variable of integration
+ * * * Variable index: 0
+ * * * Variable storage: VOI
+ */
+void SetupFixedConstants(double* CONSTANTS, double* RATES)
+{
+STATES[0] = 5;
+}
+void EvaluateVariables(double BOUND, double* CONSTANTS, double* RATES, double* STATES, double* ALGEBRAIC)
+{
+}
+void ComputeRates(double BOUND, double* STATES, double* RATES, double* CONSTANTS, double* ALGEBRAIC)
+{
+RATES[0] = -1.00000 -  - -1.00000*VOI;
+}
