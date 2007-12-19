aec359de45b49b4b379d15cd56b8f802258894e6
jmar150 <none@none>
2007-12-19 04:56:34 +0000

    Making sure that the console for gcc does not display under Vista; the behavior of inheriting flags seems to have changed from XP.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%402042

--- CIS/sources/CISImplementation.cxx
+++ CIS/sources/CISImplementation.cxx
@@ -101,7 +101,43 @@ CompileSource(std::string& destDir, std::string& sourceFile,
   cmd += targ;
   cmd += " ";
   cmd += sourceFile;
+#ifdef WIN32
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( &pi, sizeof(pi) );
+
+    // Start the child process. 
+    if( !CreateProcess( NULL,   // No module name (use command line)
+        cmd.c_str(),        // Command line
+        NULL,           // Process handle not inheritable
+        NULL,           // Thread handle not inheritable
+        FALSE,          // Set handle inheritance to FALSE
+        CREATE_NO_WINDOW,              // No visible window
+        NULL,           // Use parent's environment block
+        NULL,           // Use parent's starting directory 
+        &si,            // Pointer to STARTUPINFO structure
+        &pi )           // Pointer to PROCESS_INFORMATION structure
+    ) 
+    {
+        printf( "CreateProcess failed (%d)\n", GetLastError() );
+        return;
+    }
+
+    // Wait until child process exits.
+    WaitForSingleObject( pi.hProcess, INFINITE );
+    LPDWORD lpExitCode;
+    GetExitCodeProcess( pi.hProcess, lpExitCode );
+    int ret = lpExitCode;
+
+    // Close process and thread handles. 
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
+#else
   int ret = system(cmd.c_str());
+#endif
   if (ret != 0)
   {
     lastError = L"Could not compile the model code.";
