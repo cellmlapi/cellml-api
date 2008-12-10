7d8e44c7fec06adc670dc69d10289ca654814c01
jmar150 <none@none>
2008-12-10 23:50:49 +0000

    Progress towards Tracker Item 1153.
    --HG--
    extra : convert_revision : svn%3Aeb92df5b-a40c-0410-b02c-bf52feaaa166/CellML_DOM_API/trunk%402949

--- CIS/sources/CISImplementation.cxx
+++ CIS/sources/CISImplementation.cxx
@@ -88,7 +88,12 @@ CompileSource(std::string& destDir, std::string& sourceFile,
 #ifdef WIN32
     "-mno-cygwin -mthreads -Llib -lcis-0 "
 #else
-    "-nodefaultlibs -Llib -lcis "
+    "-nodefaultlibs "
+#ifdef __MACH__
+    "-L. -L./.libs -L./../Resources/lib -L./../Resources/components -Llib -lcellml.0 -lannotools.0 -lcuses.0 -lmalaes.0 -lcevas.0 -lcis -lccgs.0 "
+#else
+    "-Llib -lcis "
+#endif
 #endif
     "-O3 "
 #ifdef ENABLE_FAST_MATH
