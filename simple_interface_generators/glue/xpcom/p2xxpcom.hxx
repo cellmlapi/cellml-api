af6a3995181dbd264bafdd56c280426408b20fa9
Andrew Miller <ak.miller@auckland.ac.nz>
2009-06-26 13:39:17 +1200

    Correct builtin MSVC macro name to _MSC_VER

--- simple_interface_generators/glue/xpcom/p2xxpcom.hxx
+++ simple_interface_generators/glue/xpcom/p2xxpcom.hxx
@@ -8,7 +8,7 @@
 // the guard include, and set PR_STATIC_ASSERT to do nothing.
 #define prlog_h___
 #define PR_STATIC_ASSERT(x)
-#ifdef MSC_VER
+#ifdef _MSC_VER
 #undef __STDC__
 #endif
 #include <string.h>
