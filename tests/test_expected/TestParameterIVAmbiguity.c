945bc2f8e854ef7b62fc5078b746a383469e625c
Andrew Miller <ak.miller@auckland.ac.nz>
2009-11-05 14:30:23 +1300

    Added tests

---
+++ tests/test_expected/TestParameterIVAmbiguity.c
@@ -0,0 +1,10 @@
+/* Model is overconstrained.
+ * List variables defined at time of error follows...
+ * * k
+ * * k1
+ * * t
+ * * y
+ * Extraneous equation was:
+ *   3
+ *   in <math with no cmeta ID>
+ */
