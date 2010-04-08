11a19d6b86d998ec7c783b28685211373cecd403
Andrew Miller <ak.miller@auckland.ac.nz>
2010-04-08 12:12:27 +1200

    Added missing files to the Java build.

--- CIS/java_build.mk
+++ CIS/java_build.mk
@@ -14,22 +14,38 @@ cellml_jar_java += \
   $(top_srcdir)/java/cellml_bootstrap/CISBootstrap.java \
   $(top_builddir)/interfaces/pjm2pcm/cellml_services/IntegrationProgressObserver.java \
   $(top_builddir)/interfaces/pjm2pcm/cellml_services/CellMLIntegrationRun.java \
+  $(top_builddir)/interfaces/pjm2pcm/cellml_services/ODESolverRun.java \
+  $(top_builddir)/interfaces/pjm2pcm/cellml_services/DAESolverRun.java \
   $(top_builddir)/interfaces/pjm2pcm/cellml_services/CellMLCompiledModel.java \
+  $(top_builddir)/interfaces/pjm2pcm/cellml_services/ODESolverCompiledModel.java \
+  $(top_builddir)/interfaces/pjm2pcm/cellml_services/DAESolverCompiledModel.java \
   $(top_builddir)/interfaces/pjm2pcm/cellml_services/CellMLIntegrationService.java \
-  $(top_builddir)/interfaces/cellml_services/IntegrationStepType.java \
+  $(top_builddir)/interfaces/cellml_services/ODEIntegrationStepType.java \
   $(top_builddir)/interfaces/cellml_services/IntegrationProgressObserver.java \
   $(top_builddir)/interfaces/cellml_services/CellMLIntegrationRun.java \
+  $(top_builddir)/interfaces/cellml_services/ODESolverRun.java \
+  $(top_builddir)/interfaces/cellml_services/DAESolverRun.java \
   $(top_builddir)/interfaces/cellml_services/CellMLCompiledModel.java \
+  $(top_builddir)/interfaces/cellml_services/ODESolverCompiledModel.java \
+  $(top_builddir)/interfaces/cellml_services/DAESolverCompiledModel.java \
   $(top_builddir)/interfaces/cellml_services/CellMLIntegrationService.java
 
 cellml_jar_classes += \
   $(top_builddir)/javacp/cellml_bootstrap/CISBootstrap.class \
   $(top_builddir)/javacp/pjm2pcm/cellml_services/IntegrationProgressObserver.class \
   $(top_builddir)/javacp/pjm2pcm/cellml_services/CellMLIntegrationRun.class \
+  $(top_builddir)/javacp/pjm2pcm/cellml_services/ODESolverRun.class \
+  $(top_builddir)/javacp/pjm2pcm/cellml_services/DAESolverRun.class \
   $(top_builddir)/javacp/pjm2pcm/cellml_services/CellMLCompiledModel.class \
+  $(top_builddir)/javacp/pjm2pcm/cellml_services/ODESolverCompiledModel.class \
+  $(top_builddir)/javacp/pjm2pcm/cellml_services/DAESolverCompiledModel.class \
   $(top_builddir)/javacp/pjm2pcm/cellml_services/CellMLIntegrationService.class \
-  $(top_builddir)/javacp/cellml_services/IntegrationStepType.class \
+  $(top_builddir)/javacp/cellml_services/ODEIntegrationStepType.class \
   $(top_builddir)/javacp/cellml_services/IntegrationProgressObserver.class \
   $(top_builddir)/javacp/cellml_services/CellMLIntegrationRun.class \
+  $(top_builddir)/javacp/cellml_services/ODESolverRun.class \
+  $(top_builddir)/javacp/cellml_services/DAESolverRun.class \
   $(top_builddir)/javacp/cellml_services/CellMLCompiledModel.class \
+  $(top_builddir)/javacp/cellml_services/ODESolverCompiledModel.class \
+  $(top_builddir)/javacp/cellml_services/DAESolverCompiledModel.class \
   $(top_builddir)/javacp/cellml_services/CellMLIntegrationService.class
