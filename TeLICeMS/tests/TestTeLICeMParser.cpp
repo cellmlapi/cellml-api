e5fe0860aba5eb98d6ca83fafa2e55af70dd8763
Andrew Miller <ak.miller@auckland.ac.nz>
2011-01-19 16:57:42 +1300

    Add TestTeLICeMParser.cpp; update .hgignore so it doesn't hide the file anymore.

---
+++ TeLICeMS/tests/TestTeLICeMParser.cpp
@@ -0,0 +1,56 @@
+#include "Utilities.hxx"
+#include "IfaceTeLICeMS.hxx"
+#include "TeLICeMService.hpp"
+#include "IfaceCellML_APISPEC.hxx"
+#include "CellMLBootstrap.hpp"
+#include <cstdio>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
+#include <iostream>
+
+int
+main(int argc, char** argv)
+{
+  if (argc < 2)
+  {
+    puts("Usage: TestTeLICeMParser uri\n");
+    return 1;
+  }
+
+  RETURN_INTO_OBJREF(ts, iface::cellml_services::TeLICeMService,
+                     CreateTeLICeMService());
+
+  struct stat statbuf;
+  FILE* f = fopen(argv[1], "r");
+  if (f == NULL)
+  {
+    printf("Error opening file: %s\n", strerror(errno));
+    return 2;
+  }
+
+  fstat(fileno(f), &statbuf);
+  char* buf = new char[statbuf.st_size + 1];
+  fread(buf, statbuf.st_size, 1, f);
+  fclose(f);
+  buf[statbuf.st_size] = 0;
+  wchar_t* wcb = new wchar_t[statbuf.st_size + 1];
+  mbstowcs(wcb, buf, statbuf.st_size + 1);
+  delete [] buf;
+  RETURN_INTO_OBJREF(res, iface::cellml_services::TeLICeMModelResult,
+                     ts->parseModel(wcb));
+  delete [] wcb;
+  
+  RETURN_INTO_WSTRING(errmsg, res->errorMessage());
+  if (errmsg != L"")
+  {
+    fprintf(stderr, "TeLICeMS error message: %S\n", errmsg.c_str());
+    return 1;
+  }
+
+  RETURN_INTO_OBJREF(mod, iface::cellml_api::Model, res->modelResult());
+  RETURN_INTO_WSTRING(t, mod->serialisedText());
+  printf("%S\n", t.c_str());
+
+  return 0;
+}
