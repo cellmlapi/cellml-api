da485c7c298c0b29e40c22dc9ade57a011f04d46
Andrew Miller <ak.miller@auckland.ac.nz>
2011-03-30 15:49:37 +1300

    Work around compiler bug on Mac.

--- simple_interface_generators/omniidl_be/pcmpy/CToPythonWalker.py
+++ simple_interface_generators/omniidl_be/pcmpy/CToPythonWalker.py
@@ -246,7 +246,8 @@ class CToPythonWalker(idlvisitor.AstVisitor):
                 paramsigs.append('uint32_t _length_' + p.pcmname)
             paramsigs.append(p.ti.pcmType(isOut=p.is_out()) + ' ' + p.pcmname)
         paramsig = string.join(paramsigs, ', ')
-        
+        if cxxName[0:2] == '::':
+            cxxName = cxxName[2:]
         self.hxx.out('@rettype@ @cxxName@(@paramsig@) throw(std::exception&);',
                      rettype=rettype, cxxName=cxxName, paramsig=paramsig)
         self.cpp.out('@rettype@ ::@classname@::@cxxName@(@paramsig@)',
