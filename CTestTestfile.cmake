eea2afd9ea648b055e2019a789f52a699f677076
Andrew Miller <ak.miller@auckland.ac.nz>
2011-07-13 09:09:01 +1200

    Build system for more of the API.
    --HG--
    branch : use_cmake

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -5,4 +5,7 @@
 # This file includes the relevent testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 ADD_TEST(CheckCodeGenerator "tests/CheckCodeGenerator")
+ADD_TEST(CheckCIS "tests/CheckCIS")
+ADD_TEST(CheckSRuS "tests/CheckSRuS")
+ADD_TEST(CheckTeLICeMSParser "tests/CheckTeLICeMSParser")
 ADD_TEST(RunTestBin "tests/RunTestBin")
--- SRuS/srus.cmake
+++ SRuS/srus.cmake
@@ -2,12 +2,20 @@ DECLARE_EXTENSION(srus)
 DECLARE_IDL(SRuS)
 DECLARE_EXTENSION_END(srus)
 
-INCLUDE_DIRECTORIES(SRuS/sources)
+INCLUDE_DIRECTORIES(SRuS)
+INCLUDE_DIRECTORIES(xpath)
 
-ADD_LIBRARY(srus
-  SRuS/SRuSImpl.cpp)
+ADD_LIBRARY(xpath xpath/XPathImpl.cpp)
+ADD_LIBRARY(srus SRuS/SRuSImpl.cpp)
+TARGET_LINK_LIBRARIES(srus xpath cis)
 
 DECLARE_BOOTSTRAP("SRuSBootstrap" "SRuS" "Bootstrap" "SRuS" "createSRuSBootstrap" "CreateSRuSBootstrap" "SRuSBootstrap.hpp")
 
-DECLARE_CPPUNIT_FILE(SRuS)
-DECLARE_TEST_LIB(srus)
+IF (BUILD_TESTING)
+  ADD_EXECUTABLE(RunSEDML SRuS/tests/RunSEDML.cpp)
+  TARGET_LINK_LIBRARIES(RunSEDML cellml ccgs xml2 cuses cevas malaes annotools spros srus xpath)
+  ADD_TEST(CheckSRuS tests/CheckSRuS)
+  DECLARE_TEST_LIB(srus)
+  DECLARE_TEST_LIB(xpath)
+  DECLARE_CPPUNIT_FILE(XPath)
+ENDIF()
