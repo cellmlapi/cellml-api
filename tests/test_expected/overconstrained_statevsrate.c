b7a5eb58477aff9191b05ba6ae3328fca86e4a15
Andrew Miller <ak.miller@auckland.ac.nz>
2012-04-04 15:12:30 +1200

    Need to add the test files as well, fixing the build failure

---
+++ tests/test_expected/overconstrained_statevsrate.c
@@ -0,0 +1,9 @@
+/* Model is overconstrained.
+ * List variables defined at time of error follows...
+ * * d^1/dt^1 x
+ * * time
+ * * x
+ * Extraneous equation was:
+ *   <equation with no cmeta ID>
+ *   in <math with no cmeta ID>
+ */
