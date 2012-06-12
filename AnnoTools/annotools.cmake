7df68f655e77a11aa2906bdafa17ddd549398132
Andrew Miller <ak.miller@auckland.ac.nz>
2012-06-13 08:05:19 +1200

    Allow each library to have a different SOVERSION, which can be manually bumped after each interface breaking change.

--- AnnoTools/annotools.cmake
+++ AnnoTools/annotools.cmake
@@ -10,7 +10,7 @@ INCLUDE_DIRECTORIES(AnnoTools/sources)
 ADD_LIBRARY(annotools
   AnnoTools/sources/ATImplementation.cpp)
 TARGET_LINK_LIBRARIES(annotools cellml ${CMAKE_DL_LIBS})
-SET_TARGET_PROPERTIES(annotools PROPERTIES SOVERSION ${GLOBAL_SOVERSION})
+SET_TARGET_PROPERTIES(annotools PROPERTIES VERSION ${GLOBAL_VERSION} SOVERSION ${ANNOTOOLS_SOVERSION})
 INSTALL(TARGETS annotools DESTINATION lib)
 
 DECLARE_BOOTSTRAP("AnnoToolsBootstrap" "AnnoTools" "AnnotationToolService" "cellml_services" "createAnnotationToolService" "CreateAnnotationToolService" "AnnoToolsBootstrap.hpp" "AnnoTools/sources" "annotools")
