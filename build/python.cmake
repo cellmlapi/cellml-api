83a0c540ee671a1b9ddebc4088a30384c17f35cf
Andrew Miller <ak.miller@auckland.ac.nz>
2012-06-27 14:29:25 +1200

    Fix rules for building cgrspy for MinGW by passing correct path and not putting a delimiter in LIBRARY_PATH given that it doesn't seem to work with MinGW gcc.

--- build/python.cmake
+++ build/python.cmake
@@ -1,1 +1,1 @@
-ADD_CUSTOM_TARGET(buildpython ${BASH} build/make-cgrspy ${CMake_VERSION_MAJOR} ${CMake_VERSION_MINOR} ${CMAKE_BINARY_DIR})
+ADD_CUSTOM_TARGET(buildpython ${BASH} build/make-cgrspy ${CMake_VERSION_MAJOR} ${CMake_VERSION_MINOR} ${CMAKE_CURRENT_BINARY_DIR})
