c9d193fc703ad8c5ada47a41c245a0fe39f35100
Andrew Miller <ak.miller@auckland.ac.nz>
2012-07-11 16:14:45 +1200

    Fix tracker item 3326 - include changeset in generated documentation.

--- build/docs.cmake
+++ build/docs.cmake
@@ -1,5 +1,7 @@
 FIND_PACKAGE(Doxygen)
 FILE(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/docs)
-CONFIGURE_FILE(${CMAKE_SOURCE_DIR}/docs/Doxyfile.in ${CMAKE_BINARY_DIR}/docs/Doxyfile)
-ADD_CUSTOM_TARGET(docs ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/docs/Doxyfile
+CONFIGURE_FILE(${CMAKE_SOURCE_DIR}/docs/Doxyfile.in ${CMAKE_BINARY_DIR}/docs/Doxyfile.norev)
+ADD_CUSTOM_TARGET(docs 
+                  sed -e s/%PROJREV%/`git rev-parse HEAD | cut -c1-10`/ < ${CMAKE_BINARY_DIR}/docs/Doxyfile.norev >${CMAKE_BINARY_DIR}/docs/Doxyfile
+                  COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/docs/Doxyfile
                   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/docs)
