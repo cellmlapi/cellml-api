fb31fa5141d653c200c046402e6175e6afac1b15
Andrew Miller <ak.miller@auckland.ac.nz>
2013-05-06 17:26:02 +1200

    Fix Windows build

--- sources/cellml-api-cxx-support.hpp
+++ sources/cellml-api-cxx-support.hpp
@@ -5,6 +5,9 @@
 #include "Ifacexpcom.hxx"
 #include <stdlib.h>
 #include <cstring>
+#ifdef WIN32
+#include <windows.h>
+#endif
 
 HEADER_INLINE int
 CDA_objcmp(iface::XPCOM::IObject* o1, iface::XPCOM::IObject* o2)
