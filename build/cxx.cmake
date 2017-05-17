e4c737d01c69b85d8f9dab67a022704d2f07f447
Alan Garny <agarny@hellix.com>
2017-05-17 13:20:24 +0200

    Make sure that interfaces get generated before actually using them.

--- build/cxx.cmake
+++ build/cxx.cmake
@@ -1,4 +1,3 @@
-SET(CXX_IFACE_HEADER_LIST)
 SET(IDL_LIST_CXX ${IDL_LIST})
 LIST(APPEND IDL_LIST_CXX xpcom)
 FILE(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/interfaces")
@@ -6,20 +5,10 @@ INCLUDE_DIRECTORIES(${CMAKE_BINARY_DIR}/interfaces)
 FOREACH(idlfile ${IDL_LIST_CXX})
   SET(idlpath "${CMAKE_SOURCE_DIR}/interfaces/${idlfile}.idl")
   SET(headerpath "${CMAKE_BINARY_DIR}/interfaces/Iface${idlfile}.hxx")
-  ADD_CUSTOM_COMMAND(OUTPUT ${headerpath}
-                     COMMAND ${OMNIIDL} -bsimple_cpp -Iinterfaces -p${CMAKE_SOURCE_DIR}/simple_interface_generators/omniidl_be ${idlpath}
-                     MAIN_DEPENDENCY ${idlpath}
-                     DEPENDS simple_interface_generators/omniidl_be/simple_cpp/cxxheadergen.py
-                             simple_interface_generators/omniidl_be/simple_cpp/__init__.py
-                             simple_interface_generators/omniidl_be/simple_cpp/simplecxx.py
-                             simple_interface_generators/omniidl_be/simple_cpp/identifier.py
-                     WORKING_DIRECTORY interfaces
-                     VERBATIM)
-  LIST(APPEND CXX_IFACE_HEADER_LIST ${headerpath})
+  EXECUTE_PROCESS(COMMAND ${OMNIIDL} -bsimple_cpp -Iinterfaces -p${CMAKE_SOURCE_DIR}/simple_interface_generators/omniidl_be ${idlpath}
+                  WORKING_DIRECTORY interfaces)
   INSTALL(FILES ${headerpath} DESTINATION include)
 ENDFOREACH(idlfile)
-
-ADD_CUSTOM_TARGET(CXX_HEADERS_FOR_INTERFACES ALL DEPENDS ${CXX_IFACE_HEADER_LIST})
 
 FOREACH(name ${BOOTSTRAP_LIST})
   INSTALL(FILES ${BOOTSTRAP_${name}_HEADERPATH}/${BOOTSTRAP_${name}_HEADER} DESTINATION include)
